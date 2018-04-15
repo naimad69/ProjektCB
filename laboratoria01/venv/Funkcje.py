0726dd71a38a37a793ffec79f513cace5eee5d51
Damian <damianruszala@wp.pl>
2018-04-15 15:59:28 +0200

    Funkcje (15.04.18)

---
+++ laboratoria01/venv/Funkcje.py
@@ -0,0 +1,23 @@
+a=10
+b=5
+
+def dodawanie (a,b):
+    wynik = a+b
+    return wynik
+
+def odejmowanie (a,b):
+    wynik = a-b
+    return wynik
+
+def mnożenie (a,b):
+    wynik = a*b
+    return wynik
+
+def dzielenie (a,b):
+    wynik = a/b
+    return wynik
+
+print(dodawanie(a,b))
+print(odejmowanie(a,b))
+print(mnożenie(a,b))
+print(dzielenie(a,b))
