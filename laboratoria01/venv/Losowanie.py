56b39924c3f4e146ffa2de74b1ce8cebc07ce2d1
Damian <damianruszala@wp.pl>
2018-04-15 16:45:18 +0200

    Losowanie liczb (gra)

---
+++ laboratoria01/venv/Losowanie.py
@@ -0,0 +1,11 @@
+import random
+import time
+
+lista=['Wacław', 'Benito', 'Vladimir', 'Czyngishan', 'Armstrong', 'CarycaKatarzyna', 'Gebbels', 'DrOetker', 'LarraCroft', 'JanuszTracz']
+
+random.seed(time.time())
+print(random.randint(1,10))
+
+
+
+
