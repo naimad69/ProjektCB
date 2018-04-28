f7745a16eff61ebfacddd4c2be318e4c5d7630c2
Damian <damianruszala@wp.pl>
2018-04-28 13:23:19 +0200

    liczby, rozdzielone (27.04.18)

--- laboratoria01/venv/Liczby.py
+++ laboratoria01/venv/Liczby.py
@@ -4,8 +4,16 @@ plik=open('C:\\Users\\Uczeń\\Downloads\\wyniki-lotto-sortowane.csv', 'r+')
 plk=plik.readlines()
 plk.pop(0)
 
-for i in plk:
-    plk.replace('\\n', '')
+lng=len(plk)
+
+x=0
+
+while x<lng:
+    plk[x]=plk[x].replace('\n', '')
+    x=x+1
+
+for i in range(lng):
+    plk[i]=plk[i].split(';')
 
 print(plk)
 
